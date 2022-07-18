fc962d24d43b83b8539767f076401e364df66e10
emmadal <emmanueldalougou@gmail.com>
2022-07-18 00:52:17 +0000

    @apollo/client config to connect to our GraphQL server

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import env from "react-dotenv";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+} from "@apollo/client";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// @apollo/client config to connect to our GraphQL server
+const client = new ApolloClient({
+  uri: env.API_URL,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
